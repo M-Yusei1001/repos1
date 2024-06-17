4c7e370a8dbcabfaab65d44953d89e5b02ff67c9
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-06-17 22:54:47 +0900

    Practice of 240617

--- py_practice1.py
+++ py_practice1.py
@@ -1,2 +1,21 @@
-for i in range(5):
-    print(str(i) + " " +"Hello, World!")
+teams = {"Ferrari":"SF-24","RedBull":"RB20","VCARB":"VCARB01"}
+
+print("知りたいチーム名を入力してください")
+
+print("Team list:")
+for team in teams:
+    print(team)
+
+team_input = input(">")
+
+count = 0
+
+for team in teams:
+    if team == team_input:
+        print(team + "の車は" + teams[team] + "です")
+        count = 1
+    if count == 1:
+        break
+
+if count == 0:
+    print("該当するチームがありませんでした")
