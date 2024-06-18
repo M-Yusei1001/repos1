7fc9c3698bb1200c639243c500f505fb0dbbd582
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-06-18 23:48:56 +0900

    practice2

--- py_pracitce2.py
+++ py_pracitce2.py
@@ -6,27 +6,31 @@ items = {"apple":100,"orange":200,"banana":150}
 #所持金
 money = 1000
 
+#仕切り
+stick = "--------------------"
+
 print("あなたの所持金は" + str(money) + "円です")
 
 #処理
 for item in items:
-    print("--------------------")
+    print(stick)
     print("購入する" + item + "の個数を入力してください:")
     item_input = input(">")
 
     price = items[item] * int(item_input)
 
     if price < money:
-        print("購入金額は" + str(price) + "円です")
+        print("\n購入金額は" + str(price) + "円です")
         money -= price
 
     elif price == money:
-        print("購入金額は" + str(price) + "円です")
+        print("\n購入金額は" + str(price) + "円です")
         print("所持金がなくなりました")
         money -= price
         break
 
     else:
-        print("購入できませんでした")     
+        print("\n購入できませんでした")     
 
-print("\n最終的な所持金は" + str(money) + "円です")           
+print("\n" + stick)
+print("最終的な所持金は" + str(money) + "円です")           
