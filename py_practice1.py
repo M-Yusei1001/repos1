70098b2d03b124800d81a3df882d121de917745d
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-06-23 00:51:50 +0900

    practice240622

--- py_practice1.py
+++ py_practice1.py
@@ -1,6 +1,35 @@
-from utility import MenuItem
+import utility as util
 
-item = MenuItem("pizza", 500, 1000)
+print("何個商品を登録しますか？")
+num = int(input(">"))
 
-print(item)
-    
+items = []
+
+for i in range(0, num):
+    print("----------------")
+    print("情報を入力してください")
+    print("商品名,価格,カロリー\tの順で入力")
+    data = input(">").split(",")
+
+    items.append(util.MenuItem(data[0], data[1], data[2]))
+
+for i in range(0, num):
+    print("----------------")
+    print("No. " + str(i + 1))
+    items[i].info()
+
+print("----------------")
+print("何個購入しますか？")
+total_price = 0
+
+for i in range(0, num):
+    quantity = int(input(items[i].name + ">"))
+
+    if util.check_zero(quantity):
+        total_price += items[i].get_total_price(quantity)
+    else:
+        print("入力が不正です")       
+
+
+print("----------------")
+print("合計" + str(total_price) + "円です")    
--- utility.py
+++ utility.py
@@ -1,14 +1,22 @@
 class MenuItem:
     def __init__(self, name, price, calorie):
         self.name = name
-        self.price = price
-        self.calorie = calorie
+        self.price = int(price)
+        self.calorie = int(calorie)
 
     def info(self):
-        print("----------------")
         print("商品名：" + self.name)
-        print("価格：" + str(self.price))
-        print("カロリー：" + str(self.calorie))
+        print("価格：" + str(self.price) + " 円")
+        print("カロリー：" + str(self.calorie) + " kcal")
 
     def get_total_price(self, num):
-        return self.price * num  
+        if num >= 3:
+            return round(self.price * 0.9 * num)
+        else:
+            return self.price * num  
+
+def check_zero(num):
+    if num > 0:
+        return True
+    else:
+        return False
