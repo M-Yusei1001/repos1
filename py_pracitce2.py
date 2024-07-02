a3b7a2c3d439e8de83936362d57bd5553a446df6
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-07-02 23:25:51 +0900

    practice240702

--- py_pracitce2.py
+++ py_pracitce2.py
@@ -1,50 +1,25 @@
-# 対象ユーザーの注文履歴を返す処理を作成してください
-def get_order_history(USER, ORDERS):
-    order_history = []
+import requests
+from bs4 import BeautifulSoup
+import re
 
-    for ORDER in ORDERS:
-        if USER["user_id"] == ORDER["user_id"]:
-            order_history.append(ORDER)
-                
-    return order_history
+print("取得したいYahooニュース記事のURLをペーストしてください")
+url = input(">")
+
+#HTMLの取得
+response = requests.get(url)
+
+#文字化け防止
+response.encoding = response.apparent_encoding
+
+#BeautifulSoupオブジェクト作成
+soup = BeautifulSoup(response.content, "html.parser")
+
+with open("article.txt", "w", encoding="utf-8") as f:
+    #要素の抽出
+    items = soup.find_all(re.compile("article"))
+
+    for item in items:
+        f.write(item.text)
+
+print("スクレイピングは正常に終了しました")        
     
-# ここから下は触らないでください
-# 利用するデータ
-orders = [
-    {
-        'order_id': 1,
-        'user_id': 1,
-        'items': [
-            {'name': 'キャップ', 'type': 'cap', 'price': 8000},
-            {'name': 'Tシャツ', 'type': 'clothes', 'price': 2000}
-        ]
-    },
-    {
-        'order_id': 2,
-        'user_id': 2,
-        'items': [
-            {'name': 'ランニングシューズ', 'type': 'shoes', 'price': 15000},
-        ]
-    },
-    {
-        'order_id': 3,
-        'user_id': 1,
-        'items': [
-            {'name': 'スポーツドリンク', 'type': 'food', 'price': 150}
-        ]
-    },
-    {
-        'order_id': 4,
-        'user_id': 3,
-        'items': [
-            {'name': 'アンダーウェア', 'type': 'clothes', 'price': 4500},
-            {'name': 'テニスラケット', 'type': 'sports goods', 'price': 8000}          
-        ]
-    }
-]
-
-user = {'user_id': 1, 'status': 'basic'}
-# 関数の呼び出し
-user_orders = get_order_history(user, orders)
-for order in user_orders:
-    print(order)
